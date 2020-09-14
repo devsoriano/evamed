2450f9f5271590f86e551a07bd6975502ed8574e
diegoinn <diegoinn96@gmail.com>
2020-09-14 11:56:39 -0500

    Se agregó la pagina de Acerca de

--- src/app/acerca-de/acerca-de.module.ts
+++ src/app/acerca-de/acerca-de.module.ts
@@ -3,13 +3,28 @@ import { CommonModule } from '@angular/common';
 
 import { AcercaDeRoutingModule } from './acerca-de-routing.module';
 import { AcercaDeComponent } from './component/acerca-de/acerca-de.component';
+import { SharedModule } from '../shared/shared.module';
+import { MaterialModule } from './../material/material.module';
+// import { AddNewProjectComponent } from '../add-new-project/add-new-project.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [AcercaDeComponent],
+  declarations: [
+    AcercaDeComponent,
+    // AddNewProjectComponent
+  ],
   imports: [
     CommonModule,
-    AcercaDeRoutingModule
-  ]
+    SharedModule,
+    AcercaDeRoutingModule,
+    MaterialModule,
+    FormsModule
+  ],
+  exports: [
+    AcercaDeComponent,
+    // AddNewProjectComponent
+  ],
+  // entryComponents: [AddNewProjectComponent]
 })
 export class AcercaDeModule { }
