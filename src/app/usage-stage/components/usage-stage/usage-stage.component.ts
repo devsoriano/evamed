ec414447df9837dc1b8d14e02b8a2d2f079c6551
Rafael Soriano Ramírez <rafael.soriano@arteenarte.com>
2020-10-22 07:35:56 -0500

    avance de base para stages

--- src/app/usage-stage/components/usage-stage/usage-stage.component.ts
+++ src/app/usage-stage/components/usage-stage/usage-stage.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { MatListOption } from '@angular/material/list';
-import 'rxjs/add/operator/filter';
 
 @Component({
   selector: 'app-usage-stage',
@@ -10,56 +7,10 @@ import 'rxjs/add/operator/filter';
 })
 export class UsageStageComponent implements OnInit {
 
-  sheetNames: any;
-  contentData: any;
-  listData: any;
-  indexSheet: any;
-  SistemasConstructivos: any;
-  listMateriales: any;
-  selectedOptions: string[] = [];
-  panelOpenFirst = false;
-  panelOpenSecond = false;
-  panelOpenThird = false;
-
   constructor(
-    private route: ActivatedRoute,
   ) { }
 
   ngOnInit() {
-    const data = JSON.parse(sessionStorage.getItem('dataProject'));
-    this.sheetNames = data.sheetNames;
-    this.contentData = data.data;
-  }
-
-  onGroupsChange(options: MatListOption[]) {
-    let selectedSheet;
-    // map these MatListOptions to their values
-    options.map(option => {
-      selectedSheet = option.value;
-    });
-    // take index of selection
-    this.indexSheet = this.sheetNames.indexOf(selectedSheet);
-    this.listData = this.contentData[this.indexSheet];
-    // get "sistemas constructivos"
-    const sistConstructivos = [];
-    this.listData.map( sc => {
-      sistConstructivos.push(sc.Sistema_constructivo);
-    });
-    this.SistemasConstructivos = [...new Set(sistConstructivos)];
-  }
-
-  onNgModelChange(event) {
-    // console.log('on ng model change', event);
-  }
-
-  showMaterials(event, sc) {
-    const materiales = [];
-    this.listData.map( data => {
-      if (data.Sistema_constructivo === sc) {
-        materiales.push(data.Material);
-      }
-    });
-    this.listMateriales = materiales;
   }
 
 }
