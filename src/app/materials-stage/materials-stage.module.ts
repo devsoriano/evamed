3c8922a29f57a184eb8fe8880e0ce324ba75457a
Rafael Soriano Ramírez <rafael.soriano@arteenarte.com>
2020-11-29 21:05:36 -0600

    header redirect component

--- src/app/materials-stage/materials-stage.module.ts
+++ src/app/materials-stage/materials-stage.module.ts
@@ -1,19 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialsStageRoutingModule } from './materials-stage-routing.module';
 import { MaterialsStageComponent } from './components/materials-stage/materials-stage.component';
 import { SharedModule } from '../shared/shared.module';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from './../material/material.module';
+import { ProjectAndReturnComponent } from './../shared/components/project-and-return/project-and-return/project-and-return.component';
 
 @NgModule({
-  declarations: [MaterialsStageComponent],
+  declarations: [
+    MaterialsStageComponent,
+    ProjectAndReturnComponent
+  ],
   imports: [
     CommonModule,
     MaterialsStageRoutingModule,
     SharedModule,
     FormsModule,
-    MaterialModule
-  ]
+    MaterialModule,
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
 })
 export class MaterialsStageModule { }
