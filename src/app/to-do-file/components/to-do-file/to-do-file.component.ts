58282f63d04052c38860ce8ba63d525655631426
diegoinn <diegoinn96@gmail.com>
2020-12-01 15:03:20 -0600

    merger

--- src/app/to-do-file/components/to-do-file/to-do-file.component.ts
+++ src/app/to-do-file/components/to-do-file/to-do-file.component.ts
@@ -2,10 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import * as XLSX from 'xlsx';
 import { FileSaverService } from 'ngx-filesaver';
-<<<<<<< HEAD
 import { Router } from '@angular/router';
-=======
->>>>>>> 4046c7ea5f171221f21eb3982ec309aa091b028c
 
 type AOA = any[][];
 
@@ -15,7 +12,6 @@ type AOA = any[][];
   styleUrls: ['./to-do-file.component.scss'],
 })
 export class ToDoFileComponent implements OnInit {
-<<<<<<< HEAD
   fileName: string;
   nameProject: string;
 
@@ -29,17 +25,6 @@ export class ToDoFileComponent implements OnInit {
     const PDP = JSON.parse(sessionStorage.getItem('primaryDataProject'));
     this.nameProject = PDP.name_project;
   }
-=======
-  data: AOA = [];
-  fileName: string;
-
-  constructor(
-    private httpClient: HttpClient,
-    private fileSaverService: FileSaverService
-  ) {}
-
-  ngOnInit(): void {}
->>>>>>> 4046c7ea5f171221f21eb3982ec309aa091b028c
 
   onFileChange(evt: any) {
     /* wire up file reader */
@@ -53,7 +38,6 @@ export class ToDoFileComponent implements OnInit {
       /* read workbook */
       const bstr: string = e.target.result;
       const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
-<<<<<<< HEAD
       /* assingable */
       let partialData: AOA = [];
       const totalData = [];
@@ -74,14 +58,6 @@ export class ToDoFileComponent implements OnInit {
       };
       sessionStorage.setItem('dataProject', JSON.stringify(toRead));
       // this.router.navigateByUrl('materials-stage');
-=======
-      /* save data */
-      this.data = XLSX.utils.sheet_to_json(
-        wb.Sheets[wb.SheetNames[10]],
-        { raw: true, defval: null }
-      );
-      console.log(this.data);
->>>>>>> 4046c7ea5f171221f21eb3982ec309aa091b028c
     };
     reader.readAsBinaryString(target.files[0]);
   }
@@ -98,11 +74,8 @@ export class ToDoFileComponent implements OnInit {
       });
     return;
   }
-<<<<<<< HEAD
 
   saveFile() {
     this.router.navigateByUrl('materials-stage');
   }
-=======
->>>>>>> 4046c7ea5f171221f21eb3982ec309aa091b028c
 }
