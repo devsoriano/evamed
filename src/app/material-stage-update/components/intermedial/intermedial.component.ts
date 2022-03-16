5d7bcecf0473a5dde68dff557bb32785193ef73b
Rafael Soriano Ramírez <rafael.soriano@arteenarte.com>
2022-03-15 22:35:55 -0600

    add modal etapa de construcción

---
+++ src/app/material-stage-update/components/intermedial/intermedial.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { MaterialsService } from './../../../core/services/materials/materials.service';
+
+@Component({
+  selector: 'app-intermedial',
+  templateUrl: './intermedial.component.html',
+  styleUrls: ['./intermedial.component.scss'],
+})
+export class IntermedialComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private materialsService: MaterialsService,
+    public dialogRef: MatDialogRef<IntermedialComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: {}
+  ) {}
+
+  ngOnInit(): void {}
+
+  onNoClick(): void {
+    this.dialogRef.close(this.data);
+  }
+
+  continueStep(event: Event) {
+    event.preventDefault();
+    console.log('continue Step');
+    this.materialsService.getConstructionStage().subscribe((cse) => {
+      const schemaFilter = cse.filter(
+        (schema) =>
+          schema.project_id == localStorage.getItem('idProyectoConstrucción')
+      );
+      console.log(schemaFilter);
+
+      if (schemaFilter.length === 0) {
+        this.router.navigateByUrl('construction-stage');
+      } else {
+        this.router.navigateByUrl('construction-stage-update');
+      }
+    });
+    this.onNoClick();
+  }
+}
