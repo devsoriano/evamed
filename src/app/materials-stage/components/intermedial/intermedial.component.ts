f9ba90041e459ceb7db5b2a12f6c9b3f6f5b7127
Rafael Soriano Ramírez <rafael.soriano@arteenarte.com>
2022-03-16 10:26:17 -0600

    add modal en creacion de materiales

---
+++ src/app/materials-stage/components/intermedial/intermedial.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { MaterialsService } from './../../../core/services/materials/materials.service';
+
+@Component({
+  selector: 'app-intermedial',
+  templateUrl: './intermedial.component.html',
+  styleUrls: ['./intermedial.component.scss'],
+})
+export class IntermedialComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private materialsService: MaterialsService,
+    public dialogRef: MatDialogRef<IntermedialComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: {}
+  ) {}
+
+  ngOnInit(): void {}
+
+  onNoClick(): void {
+    this.dialogRef.close(this.data);
+  }
+
+  continueStep(event: Event) {
+    event.preventDefault();
+    this.onNoClick();
+  }
+}
